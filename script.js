a3b5f20fe98b271ab3defc94b0267754b6320a1b
Miao <mia0.yjz@gmail.com>
2023-05-14 11:46:04 +0800

    add wish, color to violet, height to 300px
    Signed-off-by: Miao <mia0.yjz@gmail.com>

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $(".text").hover(function() {
         $(this).toggleClass('pinklight');
     });
+    $(".wish").hover(function() {
+        $(this).toggleClass('pinklight');
+    });
 });
